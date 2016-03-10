69eaf664107e96c4775765e636a7dfd7d3b5076d
Will Morgan <wmorgan@factual.com>
2016-03-09 21:11:03 -0800

    Add movement, some legality checks

--- src/chess/core.clj
+++ src/chess/core.clj
@@ -21,14 +21,14 @@
 
 (def starting-board
   (mapv vec
-       [(back-row :white)
-        (second-row :white)
-        empty-row
-        empty-row
-        empty-row
-        empty-row
+       [(back-row :black)
         (second-row :black)
-        (back-row :black)]))
+        empty-row
+        empty-row
+        empty-row
+        empty-row
+        (second-row :white)
+        (back-row :white)]))
 
 (def starting-player
   {:previous-move nil
@@ -54,109 +54,126 @@
    {:color :black :piece :knight} \u265E
    {:color :black :piece :pawn}   \u265F})
 
-(def alg-to-indices
+(def alg-to-square
   (let [letters (zipmap "abcdefgh" (range))
         numbers (zipmap "87654321" (range))]
     (fn
       [alg]
       (reverse (map deliver [letters numbers] alg)))))
 
-(def indices-to-alg
+(def square-to-alg
   (let [letters (zipmap (range) "abcdefgh")
         numbers (zipmap (range) "87654321")]
-    (fn [indices]
-      (apply str (map deliver [letters numbers] indices)))))
+    (fn [square]
+      (apply str (map deliver [letters numbers] square)))))
 
-(assert (= "a8" (indices-to-alg [0 0])))
-(assert (= "a8" (indices-to-alg (alg-to-indices "a8"))))
-
-(defn adjacent-squares
-  [indices]
+(defn king-squares
+  [square]
   (for [i [-1 0 1]
         j [-1 0 1]
         :when (not (and (= 0 i) (= 0 j)))]
-    (map + [i j] indices)))
+    (map + [i j] square)))
 
 (defn knight-squares
-  [indices]
+  [square]
   (for [i [-2 -1 1 2]
         j [-2 -1 1 2]
         :when (odd? (+ i j))]
-    (map + [i j] indices)))
-
-(assert (= (set (knight-squares [2 2]))
-           #{[0 1]
-             [0 3]
-             [1 0]
-             [1 4]
-             [3 0]
-             [3 4]
-             [4 1]
-             [4 3]}))
+    (map + [i j] square)))
 
 (defn reachable-squares
-  [indices direction]
+  [square direction]
   (for [n (range 1 8)
-        :let [new-indices (map + indices (map #(* n %) direction))]
-        :while (every? #(<= 0 % 8) new-indices)]
-    new-indices))
+        :let [new-square (map + square (map #(* n %) direction))]
+        :while (every? #(<= 0 % 8) new-square)]
+    new-square))
 
 (defn unblocked-squares
-  [board indices direction]
+  [board square direction]
   (reduce (fn [moves position]
             (if (get-in board position)
               (reduced (conj moves position))
               (conj moves position)))
           []
-          (reachable-squares indices direction)))
+          (reachable-squares square direction)))
 
 (defn bishop-squares
-  [board indices]
+  [board square]
   (for [i [-1 1]
         j [-1 1]
-        sq (unblocked-squares board indices [i j])]
+        sq (unblocked-squares board square [i j])]
     sq))
 
 (defn rook-squares
-  [board indices]
+  [board square]
   (for [i [-1 0 1]
         j [-1 0 1]
         :when (odd? (+ i j))
-        sq (unblocked-squares board indices [i j])]
+        sq (unblocked-squares board square [i j])]
     sq))
 
 (defn queen-squares
-  [board indices]
+  [board square]
   (for [i [-1 0 1]
         j [-1 0 1]
         :when (not (and (zero? i) (zero? j)))
-        sq (unblocked-squares board indices [i j])]
+        sq (unblocked-squares board square [i j])]
     sq))
 
-(unblocked-squares starting-board [3 3] [1 1])
-(reachable-squares [3 3] [1 1])
-(bishop-squares starting-board [3 3])
-(rook-squares starting-board [3 3])
-(queen-squares starting-board [3 3])
+(defn pawn-forward
+  "Returns the list of forward square available to a pawn"
+  [square color n]
+  (map + square [(if (= :white color) (- n) n) 0]))
+
+(defn pawn-capture-squares
+  "Returns the list of forward square available to a pawn"
+  [square color]
+  (let [n (if (= :white color) -1 1)]
+    [(map + square [n -1]) (map + square [n 1])]))
+
+(def opposite {:white :black,
+               :black :white})
+
+(defn pawn-captures
+  [board square]
+  (let [{:keys [color]} (get-in board square)]
+    (filter #(= (opposite color)
+                (:color (get-in board %)))
+            (pawn-capture-squares square color))))
+
+(defn starting-rank?
+  [color [rank file]]
+  (or (and (= :white color) (= rank 6))
+      (and (= :black color) (= rank 1))))
+
+(defn pawn-squares
+  [board square]
+  (let [{:keys [color]} (get-in board square)]
+    (concat [(pawn-forward square color 1)]
+            (if (starting-rank? color square)
+              [(pawn-forward square color 2)])
+            (pawn-captures board square))))
 
 (defn moveable-squares
-  [board piece indices]
-  (cond
-    (#{:k :K} piece) (adjacent-squares indices)
-    (#{:n :N} piece) (knight-squares indices)
-    (#{:b :B} piece) (bishop-squares board indices)
-    (#{:r :R} piece) (rook-squares board indices)
-    (#{:q :Q} piece) (queen-squares board indices)))
+  [board square]
+  (let [{:keys [piece color]} (get-in board square)]
+    (cond
+      (= :pawn piece) (pawn-squares board square)
+      (= :knight piece) (knight-squares square)
+      (= :king piece) (king-squares square)
+      (= :biship piece) (bishop-squares board square)
+      (= :rook piece) (rook-squares board square)
+      (= :queen piece) (queen-squares board square))))
 
 (defn move-piece
   [move board-state]
   (-> board-state
-    (assoc-in (alg-to-indices (:from move)) nil)
-    (assoc-in (alg-to-indices (:to move)) (:piece move))))
+    (assoc-in (alg-to-square (:from move)) nil)
+    (assoc-in (alg-to-square (:to move)) (:piece move))))
 
 (defn legal?
   [move {:keys [board]}]
-  (and (= (:piece move) (get-in board (alg-to-indices (:from move))))))
+  (and (= (:piece move) (get-in board (alg-to-square (:from move))))))
 
 (defn render
   [{:keys [board]}]
@@ -166,13 +183,20 @@
 
 (defn as-html [{:keys [board]}]
   [:table#board
-   (map (fn [row offset]
+   (cons [:tr.row (for [letter " ABCDEFGH"]
+                    [:td letter])]
+   (map (fn [row offset rank]
           [:tr.row
-           (map (fn [piece color]
-                  [:td.piece {:class color}
-                   (icons piece)])
-                row offset)])
-        board (iterate next (cycle ["white" "black"])))])
+           (cons [:td rank]
+                 (map (fn [piece color]
+                        [:td.piece {:class color}
+                         (icons piece)])
+                      row offset))])
+
+        board
+        (iterate next (cycle ["white" "black"]))
+        (range 8 0 -1)
+        ))])
 
 (defn test-endpoint
   [req]
@@ -181,12 +205,36 @@
                                      :piece :P}
                                     (:board @game-state))})])
 
+(defn legal-move?
+  [board from to]
+  (some #(= (alg-to-square to) %)
+        (moveable-squares board (alg-to-square from))))
+
+(defn move!
+  [req]
+  (:return
+    (let [from (get (:params req) "from")
+          to   (get (:params req) "to")]
+      (swap! game-state
+        (fn [state]
+          (if (legal-move? (:board state) from to)
+            (assoc
+              (update state :board
+                (fn [board]
+                  (let [piece (get-in board (alg-to-square from))]
+                    (-> board
+                        (assoc-in (alg-to-square from) nil)
+                        (assoc-in (alg-to-square to) piece)))))
+              :return "OK")
+            (assoc state :return "NO")))))))
+
 (defroutes handler
   (GET "/" [] (html [:h1 "Hello World"]))
   (GET "/test" [req] (html (test-endpoint req)))
   (GET "/render" [req] (render @game-state))
   (GET "/render.html" [req] (html [:pre (render @game-state)]))
   (GET "/styled" [req] (html [:head (include-css "board.css")] [:body (as-html @game-state)]))
+  (POST "/move" [] move!)
   (route/not-found (html [:h1 "Page not found"])))
 
 (def app
